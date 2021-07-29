a302315554ed22b899d18593b5195710b2305efb
whitebear05 <woojs0505@naver.com>
2021-07-30 03:01:27 +0900

    update controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -3,12 +3,15 @@ import Writing from "../models/Writing";
 export const home = (req, res) => {
   return res.send("Home!!!!");
 };
+
 export const watch = (req, res) => {
   return res.send("Watch a Posting");
 };
+
 export const getEdit = (req, res) => {
   return res.send("Edit Page");
 };
+
 export const postEdit = (req, res) => {
   return res.send("Edit Posting");
 };
